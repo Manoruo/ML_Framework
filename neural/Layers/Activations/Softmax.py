Fixed Softmax Activation Function and minor bug fix to Cross Entropy

neural/Layers/Activations/Softmax.py
@@ -8,5 +8,11 @@ class Softmax(Activation):
         return exp_values / denom
 
     def activate_prime(self, error):
-        n = self.outputs.shape[1]
-        return np.dot((np.identity(n) - self.outputs) * self.outputs.T, error.T).T # We want final result to be in a row vecotr so take transpose
+        num_outputs = error.shape[1]
+        identity = np.eye(num_outputs) # get the diagnol matrix 
+        softmax_result = self.outputs # get whatever the output of the softmax function was (this will be a row vector and each column would be y1, y2, y3, yn)
+        return softmax_result.T * (identity - softmax_result) # transpose so we can broadcase yi to the rest of the matrix (now we have the jacobian matrix)
+
+    def backward(self, error, learning_rate):
+        # the softmax backward function produces a 2D Jacobian matrix. To find the impact of each output neron, you need to multiply the proceeding error and sum down the column to get the influence of that neuron
+        return np.sum(self.activate_prime(error) * error.T, axis=0).reshape(1, -1) # reshape to give extra dimension 

neural/Losses/CE.py
@@ -20,8 +20,8 @@ class CE(Loss):
         assert y_act.shape == y_pred.shape, f"Shape of Label ({y_act.shape}) and model output ({y_pred.shape}) must match"
         # Assuming y_pred and y_act are NumPy arrays of shape (n,) or (n, m)
         y_pred = np.maximum(y_pred, 1e-10)  # Prevent any predicted values from being 0. Basically if you're a negative or 0 value, assume thats very bad (loss should be large)
-        confidence_loss = -np.log(y_pred) # how confident our model is (we want y_pred to be a large as possible for the correct label)
-        total_loss = np.sum(y_act * confidence_loss)  # Calculate cross-entropy loss 
+        confidence_loss = np.log(y_pred) # how confident our model is (we want y_pred to be a large as possible for the correct label)
+        total_loss = -np.sum(y_act * confidence_loss)  # Calculate cross-entropy loss 
         
         # determine the number of samples
         n = len(y_act) if len(y_act.shape) > 1 else 1  
@@ -41,5 +41,6 @@ class CE(Loss):
             - gradient: Gradient of cross-entropy loss with respect to predicted probabilities (numpy array of shape (N,))
         """
 
-        #y_pred = np.maximum(y_pred, 1e-10) # prevent divide by 0 error
-        return y_pred - y_act
+        y_pred = np.maximum(y_pred, 1e-10) # prevent divide by 0 error
+        result = -y_act / (y_pred)
+        return result